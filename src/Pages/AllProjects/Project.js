395c320bfdd158fe5ad9ebfc9838d8160c0a1e4d
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-11-06 01:58:43 +0600

    another update

--- src/Pages/AllProjects/Project.js
+++ src/Pages/AllProjects/Project.js
@@ -4,11 +4,13 @@ import React from 'react';
 import './Project.css';
 
 const Project = ({ project }) => {
-    const { porject_img1, porject_img2, porject_img3, porject_name, porject_live_link, porject_client_side, porject_server_side } = project;
+    const { porject_img1, porject_img2, porject_img3, porject_name, porject_title, porject_detail_1, porject_detail_2, porject_detail_3, porject_detail_4, porject_live_link, porject_client_side, porject_server_side } = project;
 
     return (
-        <div class="col  ">
-            <div class=" h-100 project-dev-shadow">
+        <div class="col">
+
+            <div class="project-dev-shadow">
+
                 <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
@@ -22,22 +24,24 @@ const Project = ({ project }) => {
                         </div>
                     </div>
                 </div>
-                {/* ------------------ */}
-                <div className=" project-project-dev">
+
+                <div className=" projects-projects-dev">
                     <h5 className=" mt-3">{porject_name}</h5>
-                    <p className=" ">Laptop Manufacturing Website</p>
-                    <div className=""><small>
-                        <ul>
-                            <li>Laptop Store is a manufacture company warehouse.</li>
-                            <li>From here the products are delivered to the dealers.</li>
-                            <li>Many products are sold together from here.</li>
-                            <li>Retail products are not sold from here.</li>
-                        </ul>
-                    </small>
+                    <p className=" ">{porject_title}</p>
+                    <div className="">
+                        <small>
+                            <ul>
+                                <li>{porject_detail_1}</li>
+                                <li>{porject_detail_2}</li>
+                                <li>{porject_detail_3}</li>
+                                <li>{porject_detail_4}</li>
+                            </ul>
+                        </small>
                     </div>
                 </div>
-                <div className="p-2 project-project-dev">
-                    <small className="d-flex justify-content-end">
+
+                <div className="p-2 projects-projects-dev ">
+                    <small className="d-flex justify-content-end live_client_server_btn">
                         <a href={porject_live_link} target='_blank'>   <button className='project-live-btn' ><FontAwesomeIcon icon={faEye} /> Live Preview</button></a>
                         <a href={porject_client_side} target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Client</button></a>
                         <a href={porject_server_side} target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Server</button></a>
--- src/Pages/HProjects/HProjects.js
+++ src/Pages/HProjects/HProjects.js
@@ -25,132 +25,16 @@ const HProjects = () => {
 
                 <div class="row row-cols-1 row-cols-md-3 g-4 project-dev-bg">
                     {
-                        projects.slice(0,3).map(project => <Project key={project._id} project={project} />)
+                        projects.slice(0, 3).map(project => <Project key={project._id} project={project} />)
                     }
-                   {/*  <div class="col  ">
-                        <div class=" h-100 project-dev-shadow">
-                            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-                                <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore1} />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore2} />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore3} />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className=" project-project-dev">
-                                <h5 className=" mt-3">Laptop Store</h5>
-                                <p className=" ">Laptop Manufacturing Website</p>
-                                <div className=""><small>
-                                    <ul>
-                                        <li>Laptop Store is a manufacture company warehouse.</li>
-                                        <li>From here the products are delivered to the dealers.</li>
-                                        <li>Many products are sold together from here.</li>
-                                        <li>Retail products are not sold from here.</li>
-                                    </ul>
-                                </small>
-                                </div>
-                            </div>
-                            <div className="p-2 project-project-dev">
-                                <small className="d-flex justify-content-end">
-                                    <a href='https://laptop-stores.web.app/' target='_blank'>   <button className='project-live-btn' ><FontAwesomeIcon icon={faEye} /> Live Preview</button></a>
-                                    <a href='https://github.com/abdullahalmasudpersonal/Laptop-Store-Client' target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Client</button></a>
-                                    <a href='https://github.com/abdullahalmasudpersonal/laptop_store_server' target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Server</button></a>
-                                </small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col ">
-                        <div class=" h-100 project-dev-shadow">
-                            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-                                <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore1} />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore2} />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore3} />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className=" project-project-dev">
-                                <h5 className=" mt-3">Computer Accessories Warehouse</h5>
-                                <p className=" ">Computer Accessories Warehouse Website</p>
-                                <div className=""><small>
-                                    <ul>
-                                        <li>Laptop Store is a manufacture company warehouse.</li>
-                                        <li>From here the products are delivered to the dealers.</li>
-                                        <li>Many products are sold together from here.</li>
-                                        <li>Retail products are not sold from here.</li>
-                                    </ul>
-                                </small>
-                                </div>
-                            </div>
-                            <div className="p-2 project-project-dev">
-                                <small className="d-flex justify-content-end">
-                                    <a href='https://computer-accessories-warehouse.web.app/' target='_blank'>   <button className='project-live-btn' ><FontAwesomeIcon icon={faEye} /> Live Preview</button></a>
-                                    <a href='https://github.com/abdullahalmasudpersonal/Computer-Accessories-Warehouse-Client' target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Client</button></a>
-                                    <a href='https://github.com/abdullahalmasudpersonal/Computer-Accessories-Warehouse-server' target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Server</button></a>
-                                </small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col ">
-                        <div class=" h-100 project-dev-shadow">
-
-                            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-                                <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore1} />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore2} />
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img className="d-block w-100 rounded-top" src={laptopStore3} />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className=" project-project-dev">
-                                <h5 className=" mt-3">Netcom</h5>
-                                <p className=" ">Netcom Social Media Web Application</p>
-                                <div className=""><small>
-                                    <ul>
-                                        <li>Laptop Store is a manufacture company warehouse.</li>
-                                        <li>From here the products are delivered to the dealers.</li>
-                                        <li>Many products are sold together from here.</li>
-                                        <li>Retail products are not sold from here.</li>
-                                    </ul>
-                                </small>
-                                </div>
-                            </div>
-
-                            <div className="p-2 project-project-dev">
-                                <small className="d-flex justify-content-end">
-                                    <a href='https://netcom1.web.app/' target='_blank'>   <button className='project-live-btn' ><FontAwesomeIcon icon={faEye} /> Live Preview</button></a>
-                                    <a href='https://github.com/abdullahalmasudpersonal/netcom-client' target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Client</button></a>
-                                    <a href='https://github.com/abdullahalmasudpersonal/netcom-server' target='_blank'><button className='project-live-btn'> <i className="fab fa-github"></i> Server</button></a>
-                                </small>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
                 <div className='blogs-see-all-btn mt-3'>
-                        <Link to='/allprojects'>
-                            <button >
-                                See All Projects
-                            </button>
-                        </Link>
-                    </div>
+                    <Link to='/allprojects'>
+                        <button >
+                            See All Projects
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
